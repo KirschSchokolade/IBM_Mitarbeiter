31ec508e55fd2246eec762e767d09161810f4f97
sebastiankrause <sebatian.krause94@t-online.de>
2020-02-24 22:36:37 +0100

    Kommentare den aktuellen Hypothesen angepasst

--- Mitarbeiter_Visualization.py
+++ Mitarbeiter_Visualization.py
@@ -10,25 +10,24 @@ ibm_data = pd.read_csv(ibm_filepath)
 
 print(ibm_data.describe())
 
-# Das Alter hat einen Einfluss darauf, ob ein Angestellter/eine Angestellte kündigen
+# Das Alter hat einen Einfluss darauf, ob ein Angestellter/eine Angestellte kündigen wird
 sns.swarmplot(x="Attrition", y='Age', hue="Gender", data=ibm_data)  # Den merken wir uns. alternativ mit EducationField
 
-# Das Einkommen hat einen Einfluss darauf, ob ein Angestellter/eine Angestellte kündigen
+#Das Einkommen hat einen Einfluss darauf, ob Mitarbeiter kündigen im Hinblick auf Gender und Zufriedenheit
 sns.catplot(y="MonthlyIncome", x="Attrition", hue="JobSatisfaction", data=ibm_data,
             kind="swarm")
 sns.catplot(y="MonthlyIncome", x="Attrition", hue="Gender", data=ibm_data,
             kind="swarm")
 
-# In wie vielen Firmen ein Angestellter/eine Angestellte vorher gearbeitet hat gibt Hinweise, ob sie in diesem Unternehmen bleibt.
+# In wie vielen Firmen ein Angestellter vorher gearbeitet hat gibt Hinweise darauf, ob er/sie in diesem Unternehmen bleibt
 sns.catplot(y="NumCompaniesWorked", x="Attrition",hue="Gender", data=ibm_data, kind="box")
 sns.catplot(y="NumCompaniesWorked", x="Attrition", data=ibm_data, kind="box")
 
-# Wie involviert ein Angestellter/eine Angestellte in den Beruf ist und wie zufrieden ein Angestellter/eine
-# Angestellte mit diesem ist korrelieren.
+# Wie involviert ein Angestellter in den Beruf ist und seine Zufriedenheit mit dem Beruf  korrelieren
 sns.lmplot(x="JobSatisfaction", y="JobInvolvement", hue="Attrition", data=ibm_data)
 
-# Wie viele Fortbildungen ein Angestellter/eine Angestellte in letzten Jahr gemacht hat korreliert damit,
-# wie viel Geld ein Angestellter/eine Angestellte in seinem/ihrem Beruf verdient und darum damit, ob er/sie kündigt.
+#An wie viele Fortbildungen ein Angestellter im letzten Jahr teilgenommen hat korreliert damit, wie viel Geld ein Angestellter verdient und damit, ob er kündigt
+
 sns.lmplot(x="TrainingTimesLastYear", y="MonthlyIncome", data=ibm_data)
 
 plt.show()
